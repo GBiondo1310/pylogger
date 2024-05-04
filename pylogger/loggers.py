649b25874248d486bdcd4862e10df4804ab1b664
GBiondo1310 <giacomo.biondelli@gmail.com>
2024-05-04 20:48:18 +0200

    Added function recording - sab 4 mag 2024, 20:48:18, CEST

--- pylogger/loggers.py
+++ pylogger/loggers.py
@@ -39,17 +39,17 @@ class BaseLogger:
                 filter=lambda record: record["extra"]["task"] == error_task_name,
             )
 
-    def info(self, message: str) -> None:
+    def info(self, message: str, function: str) -> None:
         """Shorthand function for ``self.logger_infos.info(message)``"""
-        self.logger_infos.info(message)
+        self.logger_infos.info(f"{function}:\n: INFO : {message}\n")
 
-    def error(self, message: str) -> None:
+    def error(self, message: str, function: str) -> None:
         """Shorthand function for ``self.logger_errors.error(message)``"""
-        self.logger_errors.error(message)
+        self.logger_errors.error(f"{function}:\n: ERROR : {message}\n")
 
-    def success(self, message: str) -> None:
+    def success(self, message: str, function: str) -> None:
         """Shorthand function for ``self.logger_infos.success(message)``"""
-        self.logger_infos.success(message)
+        self.logger_infos.success(f"{function}:\n: SUCCESS : {message}\n")
 
     def status_code_success(self, status_code: int):
         self.logger_infos.success(f"Status code: [{status_code}]")
